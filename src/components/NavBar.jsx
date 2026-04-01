86f26da1f9ce9daf18068a07eb9f7e7bc4902052
one <one@url.com>
2026-04-01 13:13:53 +0200

    Added componets folder and added function for nav

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+function NavBar() {
+  return (
+    <nav className="navbar-continer">
+      <div className="links-continer">
+
+      </div>
+      <div classname="logo-continer">
+
+      </div>
+      <div className="login-continer">
+
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar
