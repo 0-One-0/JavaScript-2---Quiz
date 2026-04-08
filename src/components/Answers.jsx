63066773f2c9c6e4ef7af76a149a96125b7238fd
one <one@url.com>
2026-04-08 12:48:54 +0200

    Added new componets like timer and answers, functions moved form QuizCard to make it easier to understand. Started commenting

---
+++ src/components/Answers.jsx
@@ -0,0 +1,156 @@
+function RandomAlign({random, question, checkAns}) {
+  
+  if (random < 0.25) {
+    return (
+      <ul className="ans-continer">
+        <li
+          onClick={(e) => {
+            checkAns(e, question.correct_answer);
+          }}
+          className="options"
+        >
+          {question.correct_answer}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[0]}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[1]}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[2]}
+        </li>
+      </ul>
+    );
+  } else if (random < 0.5) {
+    return (
+      <ul className="ans-continer">
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[0]}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, question.correct_answer);
+          }}
+          className="options"
+        >
+          {question.correct_answer}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[1]}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[2]}
+        </li>
+      </ul>
+    );
+  } else if (random < 0.75) {
+    return (
+      <ul className="ans-continer">
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[0]}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[1]}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, question.correct_answer);
+          }}
+          className="options"
+        >
+          {question.correct_answer}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[2]}
+        </li>
+      </ul>
+    );
+  } else {
+    return (
+      <ul className="ans-continer">
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[0]}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[1]}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, "");
+          }}
+          className="options"
+        >
+          {question.incorrect_answers[2]}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, question.correct_answer);
+          }}
+          className="options"
+        >
+          {question.correct_answer}
+        </li>
+      </ul>
+    );
+  }
+
+ 
+}
+
+export default RandomAlign;
--- src/components/quizcard.jsx
+++ src/components/quizcard.jsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import "../quizCard.css";
+import CountdownTimer from "./Timer";
+import RandomAlign from "./Answers";
 
 gsap.registerPlugin(useGSAP);
 
+//Temp array for tesing and demo, api will be implemented. the object structure is same as api
 const quizArray = [
   {
     type: "multiple",
@@ -70,24 +73,30 @@ const quizArray = [
 ];
 
 function QuizCard() {
-  const [index, setIndex] = useState(0);
-  const [question, setQuestion] = useState(quizArray[index]);
-  const [random, setRandom] = useState(() => Math.random());
+  //All the useState are made here.
+  const [index, setIndex] = useState(0); //this is for the index that we are on like what question.
+  const [question, setQuestion] = useState(quizArray[index]); //the quiz questions
+  const [random, setRandom] = useState(() => Math.random()); //Random number to make sure that we dont have the right answer on all the questions.
   const [progress, setProgrss] = useState(
-    ((index + 1) / quizArray.length) * 100 + "%",
+    ((index + 1) / quizArray.length) * 100 + "%", //This is a useState for the progress bar, it updates with the index to giva a correct answer.
   );
-  const [answerd, setAnswerd] = useState(false);
-  const [score, setScore] = useState(0);
+  const [answerd, setAnswerd] = useState(false); //Looks the submit so that we dont leave without answering, diffrent for skip.
+  const [score, setScore] = useState(0); //Keeps check of the correct answers from user.
+  const [stopProgress, setStopProgress] = useState(false); //checks so we stop everything when the timer is done.
+  const [seconds, setSeconds] = useState(60); //use to start the timer for every question
 
+  //Checks if answer is correct or wrong, we then save score and change styles to show user. We altso lock so you can only guess ones
   const checkAns = (e, answer) => {
     if (answerd === false) {
       if (question.correct_answer === answer) {
         e.target.classList.add("correct");
         setAnswerd(true);
+        setStopProgress(true);
         setScore((prev) => prev + 1);
       } else {
         e.target.classList.add("wrong");
         setAnswerd(true);
+        setStopProgress(true);
         const answers = document.querySelectorAll(".options");
         answers.forEach((li) => {
           if (li.textContent === question.correct_answer) {
@@ -98,7 +107,7 @@ function QuizCard() {
     }
   };
 
-  let content = RandomAlign(random, question, checkAns);
+  
 
   const nextQuestion = () => {
     if (answerd === true) {
@@ -115,6 +124,8 @@ function QuizCard() {
       setIndex((prev) => prev + 1);
       setRandom(Math.random());
       setAnswerd(false);
+      setStopProgress(false);
+      setSeconds(60);
     }
   };
   const skipQuestion = () => {
@@ -131,14 +142,30 @@ function QuizCard() {
     setIndex((prev) => prev + 1);
     setRandom(Math.random());
     setAnswerd(false);
+    setStopProgress(false);
+    setSeconds(60);
   };
+
+  useEffect(() => {
+    if (stopProgress === true) {
+      setAnswerd(true);
+      const answers = document.querySelectorAll(".options");
+      answers.forEach((li) => {
+        if (li.textContent === question.correct_answer) {
+          li.classList.add("correct");
+        }
+      });
+    }
+  }, [stopProgress, question]);
 
   useEffect(() => {
     setQuestion(quizArray[index]);
   }, [index]);
+
   useEffect(() => {
     setProgrss(((index + 1) / quizArray.length) * 100 + "%");
   }, [index]);
+
   useGSAP(() => {
     gsap.to(".progressbar-indicator", {
       width: progress,
@@ -175,10 +202,19 @@ function QuizCard() {
     <div className="quiz-continer">
       <div className="quiz info">
         <div className="topper">
-          {" "}
-          <div className="back"></div>{" "}
+          <div className="back">
+            {" "}
+            <button className="back-btn">{"<"}</button>{" "}
+          </div>
           <span className="category-title">{question.category}</span>{" "}
-          <div className="timer-continer"></div>
+          <div className="timer-continer">
+            <CountdownTimer
+              setStopProgress={setStopProgress}
+              seconds={seconds}
+              setSeconds={setSeconds}
+              stopProgress={stopProgress}
+            />
+          </div>
         </div>
         <ProgressBar />
         <span className="progress-index">
@@ -189,7 +225,9 @@ function QuizCard() {
       <div className="question-continer">
         <h1 className="question">{question.question}</h1>
       </div>
-      <div className="answer-continer">{content}</div>
+      <div className="answer-continer">
+        <RandomAlign random={random} question={question} checkAns={checkAns}/>
+      </div>
       <div className="sub-continer">
         <button className="submit-btn" onClick={nextQuestion}>
           Submit
@@ -213,159 +251,7 @@ function ProgressBar() {
   );
 }
 
-function RandomAlign(random, question, checkAns) {
-  let content;
-  if (random < 0.25) {
-    content = (
-      <ul className="ans-continer">
-        <li
-          onClick={(e) => {
-            checkAns(e, question.correct_answer);
-          }}
-          className="options"
-        >
-          {question.correct_answer}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[0]}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[1]}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[2]}
-        </li>
-      </ul>
-    );
-  } else if (random < 0.5) {
-    content = (
-      <ul className="ans-continer">
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[0]}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, question.correct_answer);
-          }}
-          className="options"
-        >
-          {question.correct_answer}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[1]}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[2]}
-        </li>
-      </ul>
-    );
-  } else if (random < 0.75) {
-    content = (
-      <ul className="ans-continer">
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[0]}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[1]}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, question.correct_answer);
-          }}
-          className="options"
-        >
-          {question.correct_answer}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[2]}
-        </li>
-      </ul>
-    );
-  } else {
-    content = (
-      <ul className="ans-continer">
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[0]}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[1]}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, "");
-          }}
-          className="options"
-        >
-          {question.incorrect_answers[2]}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, question.correct_answer);
-          }}
-          className="options"
-        >
-          {question.correct_answer}
-        </li>
-      </ul>
-    );
-  }
 
-  return content;
-}
+
 
 export default QuizCard;
